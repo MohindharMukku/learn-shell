296a6a9875fd924cc635bd5d5cc035381b227d3d
mohi <mohindhar.mukku@outlook.com>
2023-05-17 11:47:14 +0530

    date and day exaple

---
+++ 02.variables.sh
@@ -0,0 +1,20 @@
+#example
+
+a=10
+name=devops
+
+#print variables
+echo a = $a
+echo name = ${name}
+
+Date=12-05-2023
+echo todays date is ${Date}
+
+#proper code
+Date=date +%f
+
+Day=date +%e
+
+echo todays date is ${Date}
+
+echo day of the month $Day
