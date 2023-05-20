74c9e5c293546681a43af622605c81781f2ffae2
mohi <mophindhar.mukku@outlook.com>
2023-05-20 18:03:16 +0530

    tv show function and condition example test only the input with average

--- 03.function.sh
+++ 03.function.sh
@@ -23,23 +23,8 @@ percent=$(curl -s  https://www.themoviedb.org/tv/$input | grep -w user_score_cha
 if [ -z "$input" ]; then
   echo tv show name is missing
 
-else [ $percent -ge 00 -a $percent -le 39 ]; then
-  echo The tv show is Disaster
-
-else [ $percent -ge 40 -a $percent -le 59 ]; then
-  echo The tv show is Flop
-
-else [ $percent -ge 60 -a $percent -le 79 ]; then
-  echo The tv show is average
-
-else [ $percent -ge 80 -a $percent -le 89 ]; then
-  echo The tv show is Above Average
-
-else [ $percent -ge 90 -a $percent -le 94 ]; then
-  echo The tv show is hit
-
-else [ $percent -ge 95 -a $percent -le 100 ]; then
-  echo The tv show is Blockbuster
+else [ $percent -ge 00 -a $percent -le 89 ]; then
+  echo The tv show Average
 
 fi
 
