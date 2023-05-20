f14028f73d733b50dcfa622ce4fc061dcd706c1a
mohi <mophindhar.mukku@outlook.com>
2023-05-20 17:38:18 +0530

    tv show function and condition example

--- 03.function.sh
+++ 03.function.sh
@@ -3,11 +3,11 @@
 #percent=code (curl -s  https://www.themoviedb.org/tv/119051-wednesday | grep -w user_score_chart | grep data-percent | xargs -n1 | grep 86 |awk -F = '{print $2}' | awk -F . '{print $1}')
 #print result is
 #   Blockbuster - 95-100
-#   hit         - 90-95
-#Above Average  - 80-90
-#Average        - 60-80
-#Flop           - 40-60
-#Disaster       - 00-40
+#   hit         - 90-94
+#Above Average  - 80-89
+#Average        - 60-79
+#Flop           - 40-59
+#Disaster       - 00-39
 
 input=$1
 if [ -z "$input" ]; then
@@ -17,9 +17,28 @@ fi
 
 percent=$(curl -s  https://www.themoviedb.org/tv/$input | grep -w user_score_chart | grep data-percent | xargs -n1 | grep 86 |awk -F = '{print $2}' | awk -F . '{print $1}')
 
+
 if [ -z "$input" ]; then
   echo tv show name is missing
-else tv show rating is $percent
+
+else [ $percent -ge 00 -a $percent -le 39 ]; then
+  echo The tv show is Disaster
+
+else [ $percent -ge 40 -a $percent -le 59 ]; then
+  echo The tv show is flop
+
+else [ $percent -ge 60 -a $percent -le 79 ]; then
+  echo The tv show is average
+
+else [ $percent -ge 80 -a $percent -le 89 ]; then
+  echo The tv show is Above Average
+
+else [ $percent -ge 90 -a $percent -le 94 ]; then
+  echo The tv show is hit
+
+else [ $percent -ge 95 -a $percent -le 100 ]; then
+  echo The tv show is Blockbuster
+
 fi
 
 
